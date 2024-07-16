4b1aeda0bbf551c5f92030bddbd761e226dde104
Piotr Drechsler <105511338+PiotrDrechsler@users.noreply.github.com>
2024-07-16 16:04:27 +0200

    Update page.tsx (#2) (#3)
    Check that the dev branch is working properly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>CI/CD Demo</h1>
+      <h1>development environment initiated and ready to work</h1>
     </main>
   );
 }
