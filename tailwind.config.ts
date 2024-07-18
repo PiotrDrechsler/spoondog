959f28962d001ef9aa8127c9f26882a939db7c3e
Asia <dobrowolska.krakow@gmail.com>
2024-07-18 17:20:58 +0200

    configured tailwind, global.css, prepared container

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,42 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        desktop: "1440px",
+      },
+      fontSize: {
+        "12": "12px",
+        "14": "14px",
+        "16": "16px",
+        "18": "18px",
+        "20": "20px",
+        "22": "22px",
+        "24": "24px",
+        "26": "26px",
+        "28": "28px",
+        "30": "30px",
+        "32": "32px",
+        "34": "34px",
+        "36": "36px",
+        "48": "48px",
+        "50": "50px",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        lobster: ["Lobster Two", "cursive"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        white: "#ffffff",
+        light: "#FFF8E3",
+        dark: "#151513",
+        grey: "#F2F2F2",
+        yellow: "#FCD503",
+        violet: "#6B6B6B",
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-btn":
+          "linear-gradient(180deg, #FCD503 14.87%, #FCB603 107.5%)",
       },
     },
   },
