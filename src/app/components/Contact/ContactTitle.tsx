da6a1aff6584485c53cc07aabf75537b9ccb1ca7
PiotrDrechsler <pdrech@gmail.com>
2024-07-28 11:39:58 +0200

    add plugin
    add pretttier plugin for talilwind
    use plugin on all components to evade future conflicts after merge

--- src/app/components/Contact/ContactTitle.tsx
+++ src/app/components/Contact/ContactTitle.tsx
@@ -4,17 +4,17 @@ import { UnderlineContactTitleMobile } from "@/app/icons/UnderlineContactTitleMo
 export const ContactTitle = () => {
   return (
     <div className="mb-[24px]">
-      <h4 className="font-semibold leading-6 mb-[16px] text-base desktop:mb-6">
+      <h4 className="mb-[16px] text-base font-semibold leading-6 desktop:mb-6">
         Kontakt
       </h4>
       <div className="relative">
-        <h2 className="text-26 font-bold tracking-[-0.04em] leading-[1.2] mb-[42px] desktop:text-48">
+        <h2 className="mb-[42px] text-26 font-bold leading-[1.2] tracking-[-0.04em] desktop:text-48">
           Skontaktuj się z nami!
         </h2>
-        <div className="hidden desktop:block absolute bottom-[-28px] left-0">
+        <div className="absolute bottom-[-28px] left-0 hidden desktop:block">
           <UnderlineContactTitleDesktop />
         </div>
-        <div className="block desktop:hidden absolute bottom-[-28px] left-0">
+        <div className="absolute bottom-[-28px] left-0 block desktop:hidden">
           <UnderlineContactTitleMobile />
         </div>
       </div>
--- src/app/components/Share/Container.tsx
+++ src/app/components/Share/Container.tsx
@@ -1,6 +1,6 @@
 const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="w-full max-w-[375px] px-[26px] mx-auto desktop:max-w-[1440px] desktop:px-[80px]">
+    <div className="mx-auto w-full max-w-[375px] px-[26px] desktop:max-w-[1440px] desktop:px-[80px]">
       {children}
     </div>
   );
