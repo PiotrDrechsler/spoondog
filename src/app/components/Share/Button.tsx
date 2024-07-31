7fa7e08202a2aef25e8b630c34be2261767e205c
PiotrDrechsler <pdrech@gmail.com>
2024-07-31 10:56:18 +0200

    fix/styles
    set styles same like on phone button
    set styles for destkop

--- src/app/components/Share/Button.tsx
+++ src/app/components/Share/Button.tsx
@@ -2,9 +2,15 @@ import { IconArrowRight } from "@/app/icons/IconArrowRight";
 
 const Button = () => {
   return (
-    <button className="btn-gradient shadow-button-shadow flex items-center rounded-full px-6 py-3 font-medium">
-      <p className="mr-3">Dowiedz się więcej</p> <IconArrowRight />
-    </button>
+    <a
+      href="/"
+      className="group flex w-max items-center gap-[10px] rounded-full bg-gradient-btn px-[24px] py-[12px] shadow-button-shadow transition duration-300 ease-in-out hover:scale-[1.05] focus:scale-[1.05] group-hover:text-violet group-focus:text-violet lg:px-[32px] lg:py-[16px]"
+    >
+      <span className="font-medium leading-[1.5] transition duration-300 ease-in-out group-hover:text-violet group-focus:text-violet lg:text-lg lg:font-semibold">
+        Dowiedz się więcej
+      </span>
+      <IconArrowRight className="size-3 fill-dark transition duration-300 ease-in-out group-hover:stroke-violet group-focus:stroke-violet lg:ml-1 lg:size-5" />
+    </a>
   );
 };
 
--- src/app/icons/IconArrowRight.tsx
+++ src/app/icons/IconArrowRight.tsx
@@ -1,18 +1,18 @@
-export const IconArrowRight = () => {
+interface IconProps {
+  className?: string;
+}
+
+export const IconArrowRight: React.FC<IconProps> = ({ className }) => {
   return (
     <svg
+      className={className}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 20"
       stroke-width="1.9"
       stroke="currentColor"
-      className="size-3"
     >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-      />
+      <path d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" fill="" />
     </svg>
   );
 };
