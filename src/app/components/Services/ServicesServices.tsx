34182e5160421137343994c297a1abc16d0780b5
Asia <dobrowolska.krakow@gmail.com>
2024-08-01 14:11:46 +0200

    responsive button correction

--- src/app/components/Services/ServicesServices.tsx
+++ src/app/components/Services/ServicesServices.tsx
@@ -9,16 +9,16 @@ export const ServicesServices = () => {
       {servicesData.map((service, index) => (
         <div
           key={index}
-          className="rounded-radius24 shadow-boxes-shadow border-greyBorders bg-cream desktop:flex-row desktop:justify-between flex w-full flex-col items-center gap-[16px] border-[1px] px-[16px] py-[28px]"
+          className="rounded-radius24 shadow-boxes-shadow border-greyBorders bg-cream desktop:flex-row flex w-full flex-col items-center gap-[16px] border-[1px] px-[16px] py-[28px]"
         >
           <div
             className={`desktop:h-[223px] desktop:w-[223px] h-[180px] w-[180px] bg-cover ${index === 0 ? "bg-services-dog1" : "bg-services-dog2"}`}
           ></div>
-          <div className="desktop:max-w-[320px] w-full">
+          <div className="desktop:max-w-[344px] w-full">
             <h3 className="text-18 desktop:mb-[8px] desktop:text-[20px] desktop:font-medium mb-[4px] font-bold leading-[1.5]">
               {service.title}
             </h3>
-            <p className="text-greyFont text-14 desktop:text-[16px] leading-[1.5]">
+            <p className="text-greyFont text-14 desktop:text-16 flex flex-wrap leading-[1.5]">
               <span>{service.label1}</span>
               <span className="mx-2">•</span>
               <span>{service.label2}</span>
@@ -29,11 +29,11 @@ export const ServicesServices = () => {
               {service.description}
             </p>
 
-            <div className="desktop:flex-row desktop:justify-between desktop:items-center flex flex-col gap-[12px]">
-              <p className="text-18 desktop:text-[24px] font-bold leading-[1.5]">
+            <div className="desktop:flex-row desktop:items-center flex flex-col gap-[12px]">
+              <p className="text-18 desktop:text-[24px] whitespace-nowrap font-bold leading-[1.5]">
                 {service.price}
               </p>
-              <div>
+              <div className="desktop:max-w-[240px] h-[46px] w-full max-w-[210px]">
                 <Button href={service.href} />
               </div>
             </div>
--- src/app/components/Share/Button.tsx
+++ src/app/components/Share/Button.tsx
@@ -8,9 +8,9 @@ const Button: React.FC<ButtonProps> = ({ href }) => {
   return (
     <a
       href={href}
-      className="w-max h-full shadow-button-shadow bg-gradient-btn group-hover:text-violet group-focus:text-violet group flex max-w-[219px] items-center gap-[10px] rounded-full px-[24px] py-[12px] transition duration-300 ease-in-out hover:scale-[1.05] focus:scale-[1.05]"
+      className="shadow-button-shadow bg-gradient-btn group-hover:text-violet group-focus:text-violet desktop:py-[12px] group flex h-full w-full items-center justify-center gap-[10px] rounded-full px-[24px] py-[9px] transition duration-300 ease-in-out hover:scale-[1.05] focus:scale-[1.05]"
     >
-      <span className="group-hover:text-violet group-focus:text-violet font-medium leading-[1.5] transition duration-300 ease-in-out">
+      <span className="group-hover:text-violet group-focus:text-violet whitespace-nowrap text-sm font-medium leading-[1.5] transition duration-300 ease-in-out md:text-base lg:text-lg">
         Dowiedz się więcej
       </span>
       <IconArrowRight className="fill-dark group-hover:stroke-violet group-focus:stroke-violet size-3 transition duration-300 ease-in-out lg:ml-1 lg:size-5" />
