69c9e02dade2ce99a6047ac56b36b6e0a9e89a7c
Agnieszka Iwan <117773931+AgnieszkaIwan@users.noreply.github.com>
2024-08-28 20:28:30 +0200

    owner - mobile (#26)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,14 @@ import { NewOffer } from "./components/NewOffer/NewOffer";
 import { Services } from "./components/Services/Services";
 import { Faq } from "./components/Faq/Faq";
 import { Hero } from "./components/Hero/Hero";
+import  SalonOwner  from "./components/SalonOwner/SalonOwner";
+
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <SalonOwner />
       <NewOffer />
       <Services />
       <Contact />
