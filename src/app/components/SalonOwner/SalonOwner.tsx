8b730bfdf8aabc30526bceee404f8c7dfb3d90af
Piotr Drechsler <105511338+PiotrDrechsler@users.noreply.github.com>
2024-09-11 10:44:30 +0200

    test/merge for dev (#28)

--- src/app/components/SalonOwner/SalonOwner.tsx
+++ src/app/components/SalonOwner/SalonOwner.tsx
@@ -1,36 +1,40 @@
-import React from 'react';
-import Image, { StaticImageData } from 'next/image';
-import owner from '@images/owner.png';
+import React from "react";
+import Image, { StaticImageData } from "next/image";
+import owner from "@images/owner.png";
 import Container from "../Share/Container";
 
-
-
 const SalonOwner: React.FC = () => {
-    return (
-        <section id="owner" className="pb-[48px] pt-[70px] desktop:pt-[88px]">
+  return (
+    <section id="owner" className="pb-[48px] pt-[70px] desktop:pt-[88px]">
       <Container>
-        <h2 className="text-left font-bold mb-4">O właścicielce salonu</h2>
-        <div className="flex justify-center mb-4">
+        <h2 className="mb-4 text-left font-bold">O właścicielce salonu...</h2>
+        <div className="mb-4 flex justify-center">
           <Image
             src={owner as StaticImageData}
             alt="Katarzyna Bober"
-            className="w-fit h-fit object-cover"
-            
+            className="h-fit w-fit object-cover"
           />
         </div>
         <div className="text-center">
           <h3 className="text-md text-left font-semibold">Katarzyna Bober</h3>
-          <p className="text-sm text-left text-gray-700 mb-4">Technik weterynarii</p>
-          <p className="text-sm text-left  text-gray-700 mb-4">
-            Katarzyna Bober, technik weterynarii i właścicielka salonu groomerskiego Spoon Dog od 2011 roku, zdobyła swoje doświadczenie w Opolskim Schronisku dla Bezdomnych Zwierząt oraz studiując zootechnikę we Wrocławiu. Posiada dyplom groomera ze szkoły Pani Ewy Grabowskiej i certyfikat Emmy Pet, co pozwala jej dbać o higienę i zdrowie psów. Jako ambasador Blue Dog edukuje dzieci i dorosłych.
+          <p className="mb-4 text-left text-sm text-gray-700">
+            Technik weterynarii
           </p>
-          <button className="bg-yellow-400 text-black py-2 px-4 rounded-full font-semibold">
+          <p className="mb-4 text-left text-sm text-gray-700">
+            Katarzyna Bober, technik weterynarii i właścicielka salonu
+            groomerskiego Spoon Dog od 2011 roku, zdobyła swoje doświadczenie w
+            Opolskim Schronisku dla Bezdomnych Zwierząt oraz studiując
+            zootechnikę we Wrocławiu. Posiada dyplom groomera ze szkoły Pani Ewy
+            Grabowskiej i certyfikat Emmy Pet, co pozwala jej dbać o higienę i
+            zdrowie psów. Jako ambasador Blue Dog edukuje dzieci i dorosłych.
+          </p>
+          <button className="bg-yellow-400 rounded-full px-4 py-2 font-semibold text-black">
             Dowiedz się więcej
           </button>
         </div>
       </Container>
-      </section>
-    );
-  };
-  
-  export default SalonOwner;
+    </section>
+  );
+};
+
+export default SalonOwner;
