5313cdedc1c7bcee6581b3561c11a6ac3694fe30
Agata Kamińska <a.fluffypie@gmail.com>
2024-09-15 18:04:09 +0200

    fix: changed max-width to 390px to be consistent with the design

--- src/app/components/Share/Container.tsx
+++ src/app/components/Share/Container.tsx
@@ -1,6 +1,6 @@
 const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mx-auto w-full max-w-[375px] px-[26px] desktop:max-w-[1440px] desktop:px-[80px]">
+    <div className="mx-auto w-full max-w-[390px] px-[26px] desktop:max-w-[1440px] desktop:px-[80px]">
       {children}
     </div>
   );
