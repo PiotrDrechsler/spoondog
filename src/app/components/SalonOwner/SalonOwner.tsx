cc250e3dea6bca1d6f56356926451d24744024a5
Agata Kamińska <a.fluffypie@gmail.com>
2024-09-15 18:01:57 +0200

    fix: fixed image display and alignment with div, revised margins for mobile, improved text alignment

--- src/app/components/SalonOwner/SalonOwner.tsx
+++ src/app/components/SalonOwner/SalonOwner.tsx
@@ -7,27 +7,27 @@ const SalonOwner: React.FC = () => {
   return (
     <section id="owner" className="pb-[48px] pt-[70px] desktop:pt-[88px]">
       <Container>
-        <p className="border-b border-x-greyLightLines pb-1 text-14 text-greyFont mb-3 desktop:border-b-0 desktop:pb-0 desktop:text-16">
+        <p className="mb-3 border-b border-x-greyLightLines pb-1 text-14 leading-6 text-greyFont desktop:border-b-0 desktop:pb-0 desktop:text-16">
           O nas / Katarzyna Bober
         </p>
-        <h2 className="mb-5 text-left text-26 font-bold desktop:text-32">
+        <h2 className="mb-10 text-left text-26 font-bold desktop:text-32">
           O właścicielce salonu
         </h2>
-        <div className="mb-4 flex justify-center">
+        <div className="mb-10 flex h-[248px] w-[342px] justify-center">
           <Image
             src={owner as StaticImageData}
             alt="Katarzyna Bober"
-            className="h-fit w-fit object-cover"
+            className="h-full w-full object-cover"
           />
         </div>
         <div className="text-center">
-          <h3 className="text-md text-left text-20 font-semibold desktop:text-26">
+          <h3 className="text-md mb-1 text-left text-20 font-semibold desktop:text-26">
             Katarzyna Bober
           </h3>
           <p className="mb-4 text-left text-16 text-sm text-gray-700 desktop:text-18">
             Technik weterynarii
           </p>
-          <p className="mb-4 text-left text-14 text-sm text-gray-700 desktop:text-16">
+          <p className="mb-4 self-stretch text-left text-14 text-sm text-gray-700 desktop:text-16">
             Katarzyna Bober, technik weterynarii i właścicielka salonu
             groomerskiego Spoon Dog od 2011 roku, zdobyła swoje doświadczenie w
             Opolskim Schronisku dla Bezdomnych Zwierząt oraz studiując
