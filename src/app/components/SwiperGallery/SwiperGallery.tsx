97971fb2fb8ba35a807ee0b38ad7143f203a0c7d
Jakub Pawlak <KubaPawlak1051@wp.pl>
2024-09-21 10:55:02 +0200

    feat: add a simple lightbox to display each gallery photo

--- src/app/components/SwiperGallery/SwiperGallery.tsx
+++ src/app/components/SwiperGallery/SwiperGallery.tsx
@@ -1,17 +1,34 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import Image, { type StaticImageData } from "next/image";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 import { swiperGalleryData } from "@/app/utils/swiperGalleryData";
 import Container from "../Share/Container";
 import { Swiper } from "../Share/Swiper/Swiper";
 import { SwiperGalleryTitle } from "./SwiperGalleryTitle";
 
+type SwiperGalleryImage = string | StaticImageData;
+
 export const SwiperGallery = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [photoIndex, setPhotoIndex] = useState(0);
+
   return (
     <section id="gallery" className="pb-12 pt-[70px] desktop:pt-[88px]">
       <Container>
         <SwiperGalleryTitle />
         <Swiper arrowVisibility="largeScreen" loop>
-          {swiperGalleryData.map((item, index) => (
-            <div key={index} className="relative aspect-square w-full">
+          {swiperGalleryData.map((item: SwiperGalleryImage, index: number) => (
+            <div
+              key={index}
+              className="relative aspect-square w-full cursor-pointer"
+              onClick={() => {
+                setPhotoIndex(index);
+                setIsOpen(true);
+              }}
+            >
               <Image
                 src={item}
                 alt={`Zdjęcie ${index + 1}`}
@@ -24,6 +41,17 @@ export const SwiperGallery = () => {
           ))}
         </Swiper>
       </Container>
+      <Lightbox
+        open={isOpen}
+        close={() => setIsOpen(false)}
+        index={photoIndex}
+        slides={swiperGalleryData.map(
+          (item: SwiperGalleryImage, index: number) => ({
+            src: typeof item === "string" ? item : item.src,
+            alt: `Zdjęcie ${index + 1}`,
+          }),
+        )}
+      />
     </section>
   );
 };
