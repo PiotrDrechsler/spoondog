bed1128a064030452400e0b30d655b82275dd4b6
Agata Kamińska <a.fluffypie@gmail.com>
2024-09-23 12:57:31 +0200

    fix: improved styles to better reflect the design

--- src/app/components/Regulations/Regulations.tsx
+++ src/app/components/Regulations/Regulations.tsx
@@ -8,12 +8,12 @@ export const Regulations = () => {
         <h2 className="mb-[26px] text-center text-24 font-bold leading-[28.8px] tracking-[-0.48px] desktop:mb-[60px] desktop:text-44 desktop:leading-[52.8px] desktop:tracking-[-1.76px]">
           Regulamin salonu Spoondog
         </h2>
-        <div className="w-full rounded-radius24 border-[1px] border-greyBorders bg-cream p-[20px] leading-[1.5] shadow-boxes-shadow desktop:p-[40px] desktop:text-18">
+        <div className="shadow-reg-shadow w-full rounded-radius24 border-[1px] border-black bg-cream px-[16px] py-[24px] text-14 leading-[21px] desktop:p-[60px] desktop:text-18 desktop:leading-[27px]">
           <ol className="list-inside list-decimal">
             {regulationsData.map((item, index) => (
               <li key={index} className="mb-[10px]">
                 {index === 0 && (
-                  <span className="font-semibold">
+                  <span className="font-medium">
                     WIZYTY UMAWIANE SĄ TELEFONICZNIE{" "}
                   </span>
                 )}
