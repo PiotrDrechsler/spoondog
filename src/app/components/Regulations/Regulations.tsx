cd07bfb20d79fbd80b6badce845a37046948f0eb
Agata Kamińska <a.fluffypie@gmail.com>
2024-09-24 11:34:29 +0200

    feat: style improvements

--- src/app/components/Regulations/Regulations.tsx
+++ src/app/components/Regulations/Regulations.tsx
@@ -3,7 +3,7 @@ import { regulationsData } from "@/app/utils/regulationsData";
 
 export const Regulations = () => {
   return (
-    <section className="pb-[48px] pt-[100px] desktop:pt-[120px]">
+    <section className="h-full w-full pb-[40px] pt-[100px] desktop:pb-[226px] desktop:pt-[120px]">
       <Container>
         <h2 className="mb-[26px] text-center text-24 font-bold leading-[28.8px] tracking-[-0.48px] desktop:mb-[60px] desktop:text-44 desktop:leading-[52.8px] desktop:tracking-[-1.76px]">
           Regulamin salonu Spoondog
@@ -13,7 +13,7 @@ export const Regulations = () => {
             {regulationsData.map((item, index) => (
               <li key={index} className="mb-[10px]">
                 {index === 0 && (
-                  <span className="font-medium mr-4 tablet:m-0">
+                  <span className="mr-4 font-medium tablet:m-0">
                     WIZYTY UMAWIANE SĄ TELEFONICZNIE -{" "}
                   </span>
                 )}
