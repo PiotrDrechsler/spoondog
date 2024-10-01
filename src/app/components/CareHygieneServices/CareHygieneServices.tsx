1d4134372db12f5747531b9f7c8c92ee80428402
Agata Kamińska <a.fluffypie@gmail.com>
2024-10-01 20:45:06 +0200

    fix: price lists removed

--- src/app/components/CareHygieneServices/CareHygieneServices.tsx
+++ src/app/components/CareHygieneServices/CareHygieneServices.tsx
@@ -40,7 +40,7 @@ export const CareHygieneServices: React.FC<CareHygieneServicesProps> = ({
           <div
             key={index}
             id={service.id}
-            className="pt-[132px] desktop:py-[40px] desktop:mt-[100px]"
+            className="pt-[132px] desktop:mt-[100px] desktop:py-[40px]"
           >
             <h4 className="mb-[4px] text-20 font-medium leading-[1.2] desktop:text-24">
               {service.title}
@@ -70,36 +70,6 @@ export const CareHygieneServices: React.FC<CareHygieneServicesProps> = ({
                   <p>{service.description1}</p>
                   <p>{service.description2}</p>
                   <p>{service.description3}</p>
-                </div>
-                <p className="mb-[8px] text-[20px] font-medium leading-[1.5]">
-                  {service.price}
-                </p>
-                <div className="mb-[24px] leading-[1.2] desktop:text-18">
-                  <p>
-                    <span className="mx-2">•</span>
-                    {service.mini}
-                    <span className="font-bold">{service.miniPrice}</span>
-                  </p>
-                  <p>
-                    <span className="mx-2">•</span>
-                    {service.small}
-                    <span className="font-bold">{service.smallPrice}</span>
-                  </p>
-                  <p>
-                    <span className="mx-2">•</span>
-                    {service.medium}
-                    <span className="font-bold">{service.mediumPrice}</span>
-                  </p>
-                  <p>
-                    <span className="mx-2">•</span>
-                    {service.big}
-                    <span className="font-bold">{service.bigPrice}</span>
-                  </p>
-                  <p>
-                    <span className="mx-2">•</span>
-                    {service.cat}
-                    <span className="font-bold">{service.catPrice}</span>
-                  </p>
                 </div>
               </div>
             </div>
