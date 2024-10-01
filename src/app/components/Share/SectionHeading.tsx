855893c1e196999a343cf157b63e4077208cd2b5
PiotrDrechsler <pdrech@gmail.com>
2024-10-01 16:05:14 +0200

    sectionMainHeading mob
    fix styles for sectionTitle
    imporove comment text
    add styles for sectionMainHeading mobile

--- src/app/components/Share/SectionHeading.tsx
+++ src/app/components/Share/SectionHeading.tsx
@@ -11,11 +11,15 @@ export const SectionHeading: React.FC<SectionHeadingProps> = ({
 }) => {
   return (
     <>
-      <p className="mb-[12px] border-b pb-[2px] leading-6 text-greyFont">
+      <p className="mb-[12px] border-b pb-[2px] text-14 leading-[1.5] text-greyFont">
         {sectionTitle}
       </p>
-      <h2>{sectionMainHeading}</h2> {/* text important for SEO */}
-      <h3>{sectionInfo}</h3> {/* text important for SEO */}
+      {/* h2 tag - important text for SEO! */}
+      <h2 className="mb-[8px] text-26 font-bold leading-[1.2] tracking-tight">
+        {sectionMainHeading}
+      </h2>
+      {/* in h3 tag - important text for SEO! */}
+      <h3>{sectionInfo}</h3>
     </>
   );
 };
