f50dbf13ba559a4ce3b413c1a537d3e22ffbef01
Piotr Drechsler <105511338+PiotrDrechsler@users.noreply.github.com>
2024-10-02 10:04:44 +0200

    feat/component/SectionHeading (#37)
    * Create SectionHeading.tsx
    
    create basic structure of component
    
    * props
    
    add props for component
    type props
    add  html tags
    comment the most important headings for SEO
    
    create test component
    
    * sectionTitle mob
    
    add styles for sectionTitle <p> mob
    
    * sectionMainHeading mob
    
    fix styles for sectionTitle
    imporove comment text
    add styles for sectionMainHeading mobile
    
    * Refactor
    
    enhance SectionHeading with ReactNode for SEO tags
    
    * desktop styles
    
    add styles for desktop
    
    * code cleaning
    
    delete code made for tests
    
    * example additon
    
    add component to SalonOwner section to show how it works :)
    
    * Update SectionHeading.tsx
    
    removal of letter spacing as suggested by Marlena and Agata
    
    * Update SalonOwner.tsx
    
    fix image position
    set h to auto for section container

---
+++ src/app/components/Share/SectionHeading.tsx
@@ -0,0 +1,29 @@
+interface SectionHeadingProps {
+  sectionTitle?: string;
+  sectionMainHeading: React.ReactNode;
+  sectionInfo: React.ReactNode; // Sometimes when you need to use tags like <strong> for better SEO for 1 word, ReactNode is a better solution.
+}
+
+export const SectionHeading: React.FC<SectionHeadingProps> = ({
+  sectionTitle = "", // Default value as an empty string
+  sectionMainHeading,
+  sectionInfo,
+}) => {
+  return (
+    <section aria-labelledby={`section-heading-for-${sectionMainHeading}`}>
+      {sectionTitle && (
+        <p className="mb-[12px] border-b border-greyLightLines pb-[2px] text-14 leading-[1.5] text-greyFont desktop:text-16">
+          {sectionTitle}
+        </p>
+      )}
+      {/* h2 tag - important text for SEO! */}
+      <h2 className="mb-[8px] text-26 font-bold leading-[1.2] desktop:text-36">
+        {sectionMainHeading}
+      </h2>
+      {/* h3 tag - important text for SEO! */}
+      <h3 className="pb-[24px] text-16 leading-[1.5] desktop:text-18">
+        {sectionInfo}
+      </h3>
+    </section>
+  );
+};
