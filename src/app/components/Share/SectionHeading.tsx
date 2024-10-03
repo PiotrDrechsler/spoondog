db9ee6e2ecec4b43b306f5fec0a3d7612dc762e2
Jakub Pawlak <KubaPawlak1051@wp.pl>
2024-10-03 21:58:07 +0200

    refactor: enhance SectionHeading with optional className and improve styling
    Add optional className prop to allow custom styling in SectionHeading.
    Make sectionInfo prop optional.
    Remove bottom border from sectionTitle on desktop view.
    Remove bottom padding from sectionInfo.

--- src/app/components/Share/SectionHeading.tsx
+++ src/app/components/Share/SectionHeading.tsx
@@ -1,18 +1,23 @@
 interface SectionHeadingProps {
   sectionTitle?: string;
   sectionMainHeading: React.ReactNode;
-  sectionInfo: React.ReactNode; // Sometimes when you need to use tags like <strong> for better SEO for 1 word, ReactNode is a better solution.
+  sectionInfo?: React.ReactNode; // Sometimes when you need to use tags like <strong> for better SEO for 1 word, ReactNode is a better solution.
+  className?: string; // Optional prop for styling
 }
 
 export const SectionHeading: React.FC<SectionHeadingProps> = ({
   sectionTitle = "", // Default value as an empty string
   sectionMainHeading,
   sectionInfo,
+  className,
 }) => {
   return (
-    <section aria-labelledby={`section-heading-for-${sectionMainHeading}`}>
+    <section
+      aria-labelledby={`section-heading-for-${sectionMainHeading}`}
+      className={className}
+    >
       {sectionTitle && (
-        <p className="mb-[12px] border-b border-greyLightLines pb-[2px] text-14 leading-[1.5] text-greyFont desktop:text-16">
+        <p className="mb-[12px] border-b border-greyLightLines pb-[2px] text-14 leading-[1.5] text-greyFont desktop:border-none desktop:text-16">
           {sectionTitle}
         </p>
       )}
@@ -21,9 +26,9 @@ export const SectionHeading: React.FC<SectionHeadingProps> = ({
         {sectionMainHeading}
       </h2>
       {/* h3 tag - important text for SEO! */}
-      <h3 className="pb-[24px] text-16 leading-[1.5] desktop:text-18">
-        {sectionInfo}
-      </h3>
+      {sectionInfo && (
+        <h3 className="text-16 leading-[1.5] desktop:text-18">{sectionInfo}</h3>
+      )}
     </section>
   );
 };
