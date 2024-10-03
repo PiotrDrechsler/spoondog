591dbe9ccfc467483a58ec4265779d94c3bca026
Jakub Pawlak <KubaPawlak1051@wp.pl>
2024-10-03 22:04:16 +0200

    refactor: use SectionHeading in home page sections
    Use SectionHeading component in Contact, Faq, SalonOwner, Services and SwiperGallery components.

--- src/app/components/SwiperGallery/SwiperGalleryTitle.tsx
+++ src/app/components/SwiperGallery/SwiperGalleryTitle.tsx
@@ -1,11 +1,10 @@
+import { SectionHeading } from "../Share/SectionHeading";
+
 export const SwiperGalleryTitle = () => (
-  <div className="mb-[40px] desktop:mb-[64px]">
-    <p className="mb-3 border-b border-x-greyLightLines pb-1 text-14 leading-6 text-greyFont desktop:border-b-0 desktop:pb-0 desktop:text-16">
-      O nas / Galeria
-    </p>
-    <h2 className="mb-2.5 text-26 font-bold leading-[1.2] tracking-[-0.02em] desktop:mb-2 desktop:text-32 desktop:font-medium">
-      Galeria
-    </h2>
-    <p className="leading-normal desktop:text-18">Zobacz efekty naszej pracy</p>
-  </div>
+  <SectionHeading
+    sectionTitle="O nas / Galeria"
+    sectionMainHeading="Galeria"
+    sectionInfo="Zobacz efekty naszej pracy"
+    className="mb-[40px] desktop:mb-[64px]"
+  />
 );
