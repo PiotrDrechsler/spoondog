329cad278ab910302009e15ebe96591d806da603
PiotrDrechsler <pdrech@gmail.com>
2024-10-04 18:04:58 +0200

    Update tailwind.config.ts
    fix name

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -47,7 +47,7 @@ const config: Config = {
         greyBorders: "#EAEAE7",
         backdrop: "rgba(0, 0, 0, 0.5)",
       },
-      rderRadius: {
+      borderRadius: {
         "footer-radius": "40px 40px 0px 0px",
         radius24: "24px",
         "header-radius": "0px 0px 12px 12px",
