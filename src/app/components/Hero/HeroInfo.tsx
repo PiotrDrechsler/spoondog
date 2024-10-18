4a42536edd0119275e8575744f9c95000b89e94b
PiotrDrechsler <pdrech@gmail.com>
2024-10-18 12:04:13 +0200

    Update HeroInfo.tsx
    delete doubled Link import to fix vercel error

--- src/app/components/Hero/HeroInfo.tsx
+++ src/app/components/Hero/HeroInfo.tsx
@@ -1,16 +1,15 @@
 import Link from "next/link";
 import { LogoFooter } from "@/app/icons/LogoFooter";
 import { HeroButtonBox } from "../Hero/HeroButtonBox";
-import Link from "next/link";
 import { HeroIcons } from "./HeroIcons";
 
 export const HeroInfo = () => {
   return (
-    <div className="mx-[18px] mt-[402px] h-[507px] w-fill tablet:mt-[151px] tablet:h-[408px] tablet:w-[230px] desktop:mt-[10px] desktop:w-[476px] xl:w-[476px]">
+    <div className="w-fill mx-[18px] mt-[402px] h-[507px] tablet:mt-[151px] tablet:h-[408px] tablet:w-[230px] desktop:mt-[10px] desktop:w-[476px] xl:w-[476px]">
       <div className="align-items flex flex-col justify-start">
         <Link
           href="/"
-          className="hidden w-auto tablet:mb-[60px] desktop:mb-[64px] tablet:block desktop:block"
+          className="hidden w-auto tablet:mb-[60px] tablet:block desktop:mb-[64px] desktop:block"
         >
           <LogoFooter />
         </Link>
@@ -19,7 +18,10 @@ export const HeroInfo = () => {
         </h1>
         <p className="mt-[16px] block desktop:text-[20px] desktop:leading-[1.5] xl:text-[20px]">
           Jeżeli chcesz aby Twój pies polubił zabiegi pielęgnacyjne to zapraszam
-          do mojego salonu. <span className="font-medium desktop:text-[20px] desktop:leading-[1.5] xl:text-[20px]">Przyjdź i przekonaj się jak działa magia gestów i porozumienia.</span>
+          do mojego salonu.{" "}
+          <span className="font-medium desktop:text-[20px] desktop:leading-[1.5] xl:text-[20px]">
+            Przyjdź i przekonaj się jak działa magia gestów i porozumienia.
+          </span>
         </p>
       </div>
       <div className="mt-[20px] flex flex-col items-center justify-center gap-[60px] tablet:hidden desktop:mt-[32px] desktop:block desktop:items-start desktop:justify-end">
