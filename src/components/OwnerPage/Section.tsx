322d1a227d79d50cc0d39fec7a72d92d3c65faa3
Jakub Pawlak <106376178+NacomiTagiera@users.noreply.github.com>
2024-10-24 20:49:06 +0200

    fix: ui text adjustments (#50)
    * fix: delete duplicated data files
    
    * fix: change FAQ answers from first-person plural to first-person singular
    
    * fix: add space after ul. abbreviation in contact and footer
    
    * fix: replace <a> tag with Next.js Link on owner page
    
    * fix: add missing comma in hygiene services data
    
    * style: make headers and spans bolder on owner page for better text highlighting
    
    * chore: remove unnecessary React imports

--- src/components/OwnerPage/Section.tsx
+++ src/components/OwnerPage/Section.tsx
@@ -1,15 +1,12 @@
-import React, { ReactNode } from 'react';
-
 interface SectionProps {
   title: string;
-  content: ReactNode;
+  content: React.ReactNode;
 }
 
 const Section: React.FC<SectionProps> = ({ title, content }) => {
   return (
     <div className="mb-[26px]">
-      <h3 className="font-medium text-[16px] leading-[24px] mb-[16px] mt-[26px] 
-    tablet:text-18 tablet:leading-[27px]">
+      <h3 className="mb-[16px] mt-[26px] text-[16px] font-semibold leading-[24px] tablet:text-18 tablet:leading-[27px]">
         {title}
       </h3>
       <div className="leading-6">{content}</div>
