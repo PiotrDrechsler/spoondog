72e1aae486981367c2108c137ed89ce0163ac687
Agata Kamińska <a.fluffypie@gmail.com>
2024-10-25 12:52:50 +0200

    feat: added logic for the mobile menu to also close with a swipe of the finger

--- src/components/Header/HeaderMobile.tsx
+++ src/components/Header/HeaderMobile.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import Link from "next/link";
 import { Clickable } from "@/utils/Types";
 import { HeaderSocialmedia } from "./HeaderSocialmedia";
@@ -15,8 +16,37 @@ export const HeaderMobile: React.FC<HeaderMobileProps> = ({
   svgOpen,
   svgClose,
 }) => {
+  const touchStart = useRef<number | null>(null);
+  const touchEnd = useRef<number | null>(null);
+
+  const handleTouchStart = (event: React.TouchEvent) => {
+    touchStart.current = event.touches[0].clientX;
+  };
+
+  const handleTouchMove = (event: React.TouchEvent) => {
+    touchEnd.current = event.touches[0].clientX;
+  };
+
+  const handleTouchEnd = () => {
+    if (touchStart.current !== null && touchEnd.current !== null) {
+      const diff = touchStart.current - touchEnd.current;
+
+      if (diff > 50) {
+        onClick();
+      }
+    }
+    touchStart.current = null;
+    touchEnd.current = null;
+  };
+
   return (
-    <div className="flex items-center justify-between py-[9px]">
+  
+    <div
+      className="flex items-center justify-between py-[9px]"
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
       <button className="flex flex-col items-center" onClick={onClick}>
         {iconOpen ? svgOpen : svgClose}
         <span className="text-12 font-bold leading-[1.5]">Menu</span>
