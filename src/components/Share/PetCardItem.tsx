46a0b697aa77929dfd77e92e0565bd948cdf47c5
PiotrDrechsler <pdrech@gmail.com>
2024-11-08 18:08:09 +0100

    aria-labels
    add aria labels for better page accessibly

--- src/components/Share/PetCardItem.tsx
+++ src/components/Share/PetCardItem.tsx
@@ -18,13 +18,17 @@ export const PetCardItem: React.FC<PetCardItemProps> = ({
   image,
 }) => {
   return (
-    <div className="flex max-w-[242px] flex-col items-center rounded-[32px] border px-[16px] py-[24px] leading-[1.5] shadow-card-shadow tablet:max-w-[275px] tablet:py-[32px] desktop:px-[32px] desktop:py-[24px]">
+    <div
+      className="flex max-w-[242px] flex-col items-center rounded-[32px] border px-[16px] py-[24px] leading-[1.5] shadow-card-shadow tablet:max-w-[275px] tablet:py-[32px] desktop:px-[32px] desktop:py-[24px]"
+      aria-label={`${name} - ${gender === "female" ? "samiczka" : "samiec"}`}
+    >
       <div className="relative h-[159px] w-[167px] desktop:h-[180px] desktop:w-[180px]">
         <Image
           src={image || ""}
           alt={name}
           fill
           className="rounded-[24px] object-cover"
+          aria-label={name}
         />
       </div>
       <div className="my-[16px] flex w-full justify-between">
@@ -34,14 +38,16 @@ export const PetCardItem: React.FC<PetCardItemProps> = ({
             gender === "female" ? "fill-[#F576E0]" : "fill-[#80CAFF]",
             "stroke-dark",
           )}
+          aria-label={gender === "female" ? "samiczka" : "samiec"}
         >
-          <IconPetGender className="size-[26px]" />
+          <IconPetGender className="size-[26px]" aria-label="Ikonka płci" />
         </div>
       </div>
       <Button
         content="Poznaj historie"
         href="/"
         className="py-3 desktop:py-3"
+        aria-label="Idź do strony z historią tego zwierzaka"
       />
     </div>
   );
