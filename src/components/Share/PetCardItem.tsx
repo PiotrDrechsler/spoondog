6664e4432b9628dbbf1a9f63ada7993cf0d90397
Piotr Drechsler <105511338+PiotrDrechsler@users.noreply.github.com>
2024-11-10 20:53:48 +0100

    Share/pet card item (#58)
    * add pictures
    
    add pictures to public/images
    create folder structure
    add correct names
    
    * Create fromLoveData.ts
    
    add data arrays with pets
    
    * Create PetCardItem.tsx
    
    create basic component structure
    
    * add typing
    
    add typing for component
    create component for test with static props
    
    * typing cd.
    
    add sex and href into typing and props
    update data with pets
    
    * fix for one photo
    
    fix to one photo for component
    
    * add zod
    
    add zod lib to keep safe data for component
    
    * maping logic
    
    add mapping logic and data validation
    
    * fix error with wrong src path to img
    
    * image
    
    add image resolutions for mobile and tablet + devices
    
    * fix images
    
    fix image resolutions for mobile: smaller tablet:smaller desktop:bigger
    
    * styles
    
    add basic styling for component
    
    * add icon with gender
    
    add icon with gender
    add fill logic
    add paddings for RWD
    
    * fix bugs
    
    1. add representative images as image no 1
    2. fix problem with path in paw vector
    
    * fix button
    
    add classname prop to button
    add cn to PetCardItem logic
    
    * aria-labels
    
    add aria labels for better page accessibly
    
    * code cleaning
    
    delete code added for tests
    improving several file names for better organisation of data
    
    * fix aria-labels
    
    aria label amendments for improved accessibility
    
    * Update README.md
    
    add readmeinstruction
    
    * Update README.md
    
    fix quotes
    
    * type PetProps
    
    add PetProps in utils
    add feamle/male choose
    import PetProps to petsData and PetCardItem and test PetProps in PetCardItem imported component
    
    * fix typo
    
    * Improve gender icon accessibility with figure/figcaption
    
    * Refactor PetCardItem: Extract reusable ariaLabelText constant
    
    * Update page.tsx
    
    code cleaning

---
+++ src/components/Share/PetCardItem.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import { z } from "zod";
+import Button from "./Button";
+import { IconPetGender } from "@/icons/IconPetGender";
+import { cn } from "@/utils/helpers";
+
+const petCardItemSchema = z.object({
+  name: z.string(),
+  gender: z.enum(["female", "male"]),
+  image: z.string().url(),
+});
+
+export type PetProps = z.infer<typeof petCardItemSchema>;
+
+export const PetCardItem: React.FC<PetProps> = ({ name, gender, image }) => {
+  const ariaLabelText = `${name} - ${gender === "female" ? "samiczka" : "samiec"}`;
+
+  return (
+    <div
+      className="flex max-w-[242px] flex-col items-center rounded-[32px] border px-[16px] py-[24px] leading-[1.5] shadow-card-shadow tablet:max-w-[275px] tablet:py-[32px] desktop:px-[32px] desktop:py-[24px]"
+      aria-label={ariaLabelText}
+    >
+      <div className="relative h-[159px] w-[167px] desktop:h-[180px] desktop:w-[180px]">
+        <Image
+          src={image || ""}
+          alt={name}
+          fill
+          className="rounded-[24px] object-cover"
+          aria-label={name}
+        />
+      </div>
+      <div className="my-[16px] flex w-full justify-between">
+        <p id={`pet-name-${name}`} className="text-18 font-bold tablet:text-20">
+          {name}
+        </p>
+        <figure
+          className={cn(
+            gender === "female" ? "fill-[#F576E0]" : "fill-[#80CAFF]",
+            "stroke-dark",
+          )}
+        >
+          <IconPetGender className="size-[26px]" aria-hidden="true" />
+          <figcaption className="sr-only">{ariaLabelText}</figcaption>
+        </figure>
+      </div>
+      <Button
+        content="Poznaj historię"
+        href="/"
+        className="py-3 desktop:py-3"
+        aria-label={`Idź do strony z historią zwierzaka ${name}`}
+      />
+    </div>
+  );
+};
+
+export default PetCardItem;
