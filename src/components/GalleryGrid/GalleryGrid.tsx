b9f3782a467d60a3261fc3ad84d7c51945db7400
Piotr Drechsler <105511338+PiotrDrechsler@users.noreply.github.com>
2024-11-26 16:48:27 +0100

    Fix/gallery (#64)
    * feat: add pagination to GalleryGrid component
    
    Implemented pagination for the GalleryGrid component, allowing users to navigate through gallery items. Added state management for the current page and dynamically displayed items based on the selected page. Included "Next" and "Previous" buttons with shared styles for easier reusability and maintenance.
    
    * add teeth and hairstyle data
    
    add two data folders
    refactor gallerygrid component to add prop file with data and tilte for gallery
    
    * image height
    
    fix visual bug with too high image in the lightbox fixed
    
    * Update page.tsx
    
    fix vercel error
    
    * Update GalleryGrid.tsx
    
    fix vercel error
    
    * Fix: adjust grid layout and spacing for responsive design
    
    Updated grid layout and spacing for better responsiveness. Adjusted column gaps and row gaps for desktop and tablet viewports, and modified grid height to improve consistency across devices.
    
    * Enhance: set image quality to 40 for optimized performance
    
    Added the 'quality' property with a value of 40 to the Image component to balance image quality and performance. This adjustment helps reduce load times while maintaining acceptable visual fidelity.
    
    * Feat: add shimmer effect as image placeholder
    
    Introduced a shimmer effect for the Image component placeholder to enhance the user experience during image loading. Added utility functions 'toBase64' for encoding and 'shimmer' for generating the SVG placeholder. Integrated the placeholder using the 'data:image/svg+xml;base64' format.
    
    * Update: Hairstyle Gallery
    
    add new photos to galleryGridHairstyleDate
    
    * Update: Swiper Gallery
    
    add new photos to swiperGalleryData
    
    * Update: Hygiene Services
    
    add new photo to hygieneServicesData

--- src/components/GalleryGrid/GalleryGrid.tsx
+++ src/components/GalleryGrid/GalleryGrid.tsx
@@ -1,39 +1,91 @@
 "use client";
 
+import { useState } from "react";
 import { useLightbox } from "@/hooks/useLightbox";
-import { galleryGridData } from "@/data/galleryGridData";
 import Container from "../Share/Container";
 import { LightboxGallery } from "../Share/LightboxGallery";
 import { GalleryGridItem } from "./GalleryGridItem";
 import { GalleryGridTitle } from "./GalleryGridTitle";
 
-export const GalleryGrid = () => {
+const ITEMS_PER_PAGE = 8; // Liczba elementów na stronę
+
+export interface GalleryGridProps {
+  galleryTitle: string;
+  galleryData: string[];
+}
+
+export const GalleryGrid = ({
+  galleryTitle,
+  galleryData,
+}: GalleryGridProps) => {
   const { isOpen, photoIndex, openLightbox, closeLightbox } = useLightbox();
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(galleryData.length / ITEMS_PER_PAGE);
+
+  const currentData = galleryData.slice(
+    (currentPage - 1) * ITEMS_PER_PAGE,
+    currentPage * ITEMS_PER_PAGE,
+  );
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prev) => prev - 1);
+    }
+  };
+
+  const buttonClassName =
+    "items-center justify-center gap-[10px] rounded-full bg-gradient-btn px-[20px] py-[12px] shadow-button-shadow transition duration-300 ease-in-out hover:scale-[1.05] hover:text-violet focus:scale-[1.05] focus:text-violet desktop:px-[32px] desktop:py-[16px]";
 
   return (
-    <section
-      id="gallery-grid"
-      className="pb-10 pt-[100px] desktop:pb-[140px] desktop:pt-[220px]"
-    >
-      <GalleryGridTitle />
+    <section id="gallery-grid" className="pt-[100px]">
       <Container>
-        <ul className="grid grid-cols-4 gap-x-2.5 gap-y-3 desktop:gap-x-5 desktop:gap-y-[60px]">
-          {galleryGridData.map((item: string, index: number) => (
+        <h2 className="my-8 text-center text-24 font-medium leading-[1.2] tracking-[-0.02em] desktop:text-44 desktop:font-bold">
+          {galleryTitle}
+        </h2>
+        <ul className="grid h-[170px] grid-cols-4 gap-x-2.5 gap-y-3 tablet:h-[400px] desktop:h-[650px] desktop:gap-x-5 desktop:gap-y-[60px]">
+          {currentData.map((item: string, index: number) => (
             <GalleryGridItem
               key={index}
               imageUrl={item}
-              altText={`gallery image ${index + 1}`}
+              altText={`gallery image ${(currentPage - 1) * ITEMS_PER_PAGE + index + 1}`}
               priority={index < 4}
-              onClick={() => openLightbox(index)}
+              onClick={() =>
+                openLightbox((currentPage - 1) * ITEMS_PER_PAGE + index)
+              }
             />
           ))}
         </ul>
+        <div className="mt-8 flex items-center justify-center">
+          <button
+            onClick={handlePrevPage}
+            disabled={currentPage === 1}
+            className={buttonClassName}
+          >
+            Wróć
+          </button>
+          <span className="px-4 py-2">{`${currentPage} / ${totalPages}`}</span>
+          <button
+            onClick={handleNextPage}
+            disabled={currentPage === totalPages}
+            className={buttonClassName}
+          >
+            Dalej
+          </button>
+        </div>
       </Container>
+
       <LightboxGallery
         isOpen={isOpen}
         onClose={closeLightbox}
         index={photoIndex}
-        slides={galleryGridData}
+        slides={galleryData}
       />
     </section>
   );
--- src/data/swiperGalleryData.ts
+++ src/data/swiperGalleryData.ts
@@ -1,17 +1,23 @@
-import bath from "@images/hygieneServices/bathDog1.jpg";
-import claws from "@images/hygieneServices/clawsDog1.jpg";
-import combing from "@images/careServices/combingDog1.jpg";
-import ears from "@images/hygieneServices/earsDog1.jpg";
-import haircut from "@images/careServices/haircutDog1.jpg";
-import trimming from "@images/careServices/trimmingDog1.jpg";
-import trimming2 from "@images/careServices/trimmingDog2.jpg";
+import swiper01 from "@images/gallery/landing/swiper01.jpeg";
+import swiper02 from "@images/gallery/landing/swiper02.jpeg";
+import swiper03 from "@images/gallery/landing/swiper03.jpeg";
+import swiper04 from "@images/gallery/landing/swiper04.jpeg";
+import swiper05 from "@images/gallery/landing/swiper05.jpeg";
+import swiper06 from "@images/gallery/landing/swiper06.jpeg";
+import swiper07 from "@images/gallery/landing/swiper07.jpeg";
+import swiper08 from "@images/gallery/landing/swiper08.jpeg";
+import swiper09 from "@images/gallery/landing/swiper09.jpeg";
+import swiper10 from "@images/gallery/landing/swiper10.jpeg";
 
 export const swiperGalleryData = [
-  combing,
-  haircut,
-  trimming,
-  ears,
-  bath,
-  trimming2,
-  claws,
+  swiper01,
+  swiper02,
+  swiper03,
+  swiper04,
+  swiper05,
+  swiper06,
+  swiper07,
+  swiper08,
+  swiper09,
+  swiper10,
 ];
