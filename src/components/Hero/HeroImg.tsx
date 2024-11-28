0eef2808b266df5764e4d4f446db0392a7407835
Piotr Drechsler <105511338+PiotrDrechsler@users.noreply.github.com>
2024-11-28 10:11:45 +0100

    Fix/general (#66)
    * feat/favicon
    
    add favicon with logo
    
    * fix/hero position
    
    improved positioning of icons in the hero section for better visual effect
    
    * fix/og-image
    
    add new og-image with better positioning
    delete unused icons
    
    * fix/navigation href
    
    disable href on "o nas" label for better user experience
    
    * feat: add hover and leave event handlers for navigation menu
    
    Added onMouseEnter to toggle the submenu visibility for index 1 on hover.
    Implemented onMouseLeave for submenu to handle user interactions when the mouse leaves the submenu.
    Adjusted submenu styling and ensured proper positioning for desktop view.
    
    * change/hygieneServicesData
    
    change photo for client's request

--- src/components/Hero/HeroImg.tsx
+++ src/components/Hero/HeroImg.tsx
@@ -3,20 +3,19 @@ import heroDog from "@images/heroDog.png";
 
 export const HeroImg = () => {
   return (
-    <div className="relative top-[80px] right-0 desktop:top-[10px] h-full max-h-[700px]">
-    <div className="absolute right-0 h-[288px] w-[306px] rounded-hero-radius bg-hero-bg shadow-hero-shadow tablet:h-[450px] tablet:w-[450px] desktop:h-[730px] desktop:w-[700px] xl:h-[900px] xl:w-[900px] xl:right-[80px] xl:rounded-hero-radius-big">
-      <div className="absolute bottom-0 right-0 h-[280px] w-[280px] -rotate-90 tablet:h-[430px] tablet:w-[430px] desktop:h-[700px] desktop:w-[700px] xl:h-[800px] xl:w-[800px] xl:right-[0px] xl:bottom-[0px] xl:rounded-hero-radius-dog overflow-hidden
-      ">
-        <Image
-          src={heroDog}
-          alt="Dog image"
-          fill
-          sizes="(max-width: 768px) 100vw, (min-width: 769px) 50vw"
-          priority
-          className="object-cover"
-        />
+    <div className="relative right-0 top-[80px] h-full max-h-[700px] desktop:-top-[100px]">
+      <div className="absolute right-0 h-[288px] w-[306px] rounded-hero-radius bg-hero-bg shadow-hero-shadow tablet:h-[450px] tablet:w-[450px] desktop:h-[730px] desktop:w-[700px] xl:right-[80px] xl:h-[900px] xl:w-[900px] xl:rounded-hero-radius-big">
+        <div className="absolute bottom-0 right-0 h-[280px] w-[280px] -rotate-90 overflow-hidden tablet:h-[430px] tablet:w-[430px] desktop:h-[700px] desktop:w-[700px] xl:bottom-[0px] xl:right-[0px] xl:h-[800px] xl:w-[800px] xl:rounded-hero-radius-dog">
+          <Image
+            src={heroDog}
+            alt="Dog image"
+            fill
+            sizes="(max-width: 768px) 100vw, (min-width: 769px) 50vw"
+            priority
+            className="object-cover"
+          />
+        </div>
       </div>
     </div>
-  </div>
   );
 };
--- src/data/navigationHeaderData.ts
+++ src/data/navigationHeaderData.ts
@@ -1,6 +1,6 @@
 export const navigationHeaderData = [
   { href: "/", label: "Start" },
-  { href: "/#about", label: "O nas" },
+  { href: "", label: "O nas" },
   // { href: "/#price", label: "Cennik" },
   { href: "/#contact", label: "Kontakt" },
   { href: "/#faq", label: "FAQ" },
