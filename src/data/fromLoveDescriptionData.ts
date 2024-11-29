ccc8c73f51979f8b952a298f5c8038a780358fc9
Agnieszka Iwan <117773931+AgnieszkaIwan@users.noreply.github.com>
2024-11-29 21:16:56 +0100

    data file adding

---
+++ src/data/fromLoveDescriptionData.ts
@@ -0,0 +1,30 @@
+export const contentData = {
+  quote:
+    "Adoptując jednego psa, nie zmienisz całego świata... Ale cały świat zmieni się dla tego jednego psa!",
+  adoptionEncouragement:
+    "Aby zachęcić do adopcji oferuję darmowe strzyżenie i kąpiel dla każdego psiaka, adoptowanego ze schroniska w Opolu (należy pokazać umowę adopcyjną).",
+  sections: [
+    {
+      paragraphs: [
+        "Dla odmiany nie hoduję psów, nie jestem zwolenniczką bezmyślnego rozmnażania, chciałabym móc zamknąć wszystkie pseudohodowle, gdzie zwierzęta doznają samych cierpień, służą za maszynki do robienia pieniędzy. Trzymane są w fatalnych warunkach, często bez kontaktu z ludźmi, rodzą dwa razy do roku, a jak są już stare i niepotrzebne, lądują w schroniskach lub giną. Szczeniaki z takich miejsc nie potrafią się odnaleźć w nowym domu, często do końca życia są zlęknione i nieufne, a nawet agresywne. Socjalizacja jest ważna od samego początku. Dlatego zachęcam do przemyślanych decyzji, kupna szczeniaków z prawdziwych hodowli, gdzie psiak posiada metryczkę. ",
+        "Szczególnie namawiam do adopcji, każda rasa posiada swoją fundację. Co prawda nie zawsze są to psy rasowe, często chorei po przejściach, ale swoim małym serduszkiem kochają naprawdę mocno.Są wdzięczne i wierne",
+        "Pamiętajmy jednak, że adopcja psa jest wspaniałym gestem dobrego człowieka, ale przemyślana adopcja jest czymś więcej, bo przejawia odpowiedzialność tego dobrego człowieka.",
+        "",
+      ],
+    },
+    {
+      paragraphs: [
+        "Namawiam także do pomocy bezdomnym zwierzętom na różne sposoby. Często nie wiemy co zrobić ze starymi kocami czy pościelą, którą można zawieźć do pobliskiego schroniska, lub przynieść do mnie do salonu, a na pewno przekażę psiakom.",
+        "W ramach rozliczenia ofiarować 1% podatku lub raz na jakiś czas zawieźć karmę, smakołyki czy zabawki. W schroniskach przyda się wszystko. Potrzebni są także wolontariusze. Nie przechodźmy obojętnie, jeżeli widzimy cierpiące zwierzę, reagujmy jeżeli ktoś się znęca (brak wody, krótki łańcuch czy bicie psa)lub widzimy błąkającego się zwierzaka. Służby, które powinny zareagować to straż miejska i policja, jeżeli to nie poskutkuje są fundacje, które na pewno pomogą np.TOZ.",
+        "Jeżeli decydujesz się na psa poczytaj o rasie, która Cię interesuje, zastanów czy możesz zapewnić warunki jakich potrzebuje. Każdy pies wymaga indywidualnego traktowanie, opieki i czasu.",
+        "Pamiętajmy także, że żaden pies nie jest od razu idealnym kompanem trzeba go wszystkiego nauczyć co wymaga czasu i cierpliwości.",
+      ],
+    },
+  ],
+  finalQuote:
+    "Aby właściwie nacieszyć się psem, nie wystarczy po prostu nauczyć go być prawie człowiekiem. Chodzi o to, by otworzyć się na możliwość stania się po części psem",
+  finalAuthor: "Edward Hoagland",
+  closingText:
+    "Zawsze służę pomocą w adopcjach i problemach z nimi związanymi. Pomogę każdemu kto chce pomóc naszym mniejszym braciom. Bo dla mnie praca ze zwierzętami to przyjemność, stać mnie na bezinteresowność i życzę tego każdemu.",
+  closingAuthor: "Katarzyna Bober",
+};
