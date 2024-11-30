a32cc0918c5d42c028411f51f4b7142f2753b38a
Agata Kamińska <delicadency@gmail.com>
2024-11-30 19:00:10 +0100

    feat: a blunt component structure was created

--- src/components/PetHistoryItem/PetHistoryItem.tsx
+++ src/components/PetHistoryItem/PetHistoryItem.tsx
@@ -1,3 +1,34 @@
-export const PetHistoryItem = ({ name }: { name: string }) => {
-  return <div className="capitalize">{name}</div>;
+import Container from "../Share/Container";
+import { SectionHeading } from "../Share/SectionHeading";
+
+export const PetHistoryItem = ({
+  name,
+  description,
+}: {
+  name: string;
+  description: string;
+}) => {
+  return (
+    <section className="pb-[48px] pt-[70px] desktop:gap-[60px] desktop:pt-[88px]">
+      <Container>
+        <div className="flex flex-col gap-10 desktop:gap-[60px]">
+          <SectionHeading
+            sectionTitle="Z miłości"
+            sectionMainHeading="Historie naszych zwierząt"
+            className="p-5 desktop:p-0"
+          />
+          <article>
+            <h4 className="text-20 font-medium capitalize desktop:text-28">
+              {name}
+            </h4>
+            <div className="h-[1px] w-full bg-yellow desktop:mb-[70px]"></div>
+            <div>
+              <h5>Kilka słów o </h5>
+              <p>{description}</p>
+            </div>
+          </article>
+        </div>
+      </Container>
+    </section>
+  );
 };
