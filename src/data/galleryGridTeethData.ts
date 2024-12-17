17eae9f7cb13f0090966fca7c77a485cbbded6a5
Agata Kamińska <delicadency@gmail.com>
2024-12-17 16:56:56 +0100

    fix: changed gallery array iteration

--- src/data/galleryGridTeethData.ts
+++ src/data/galleryGridTeethData.ts
@@ -1,7 +1,7 @@
 export const galleryGridTeethData = [
   ...Array.from(
     { length: 20 }, // Adjust the number as needed
-    (_, i) => `/images/gallery/teeth/gallery(${20 - i}).jpeg`,
+    (_, i) => `/images/gallery/teeth/gallery(${19 - i}).jpeg`,
   ),
 ];
 //Remember, when adding new teeth photos, add clean one first. With reverse iteration, the gallery will display dirty first, then clean :)
