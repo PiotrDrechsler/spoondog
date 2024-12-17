46f2446784456c60e34d016c989f14cc77e79195
Agata Kamińska <delicadency@gmail.com>
2024-12-17 16:30:57 +0100

    feat: added new photos to hairstyle gallery, removed duplicate

--- src/data/galleryGridHairstyeData.ts
+++ src/data/galleryGridHairstyeData.ts
@@ -1,6 +1,6 @@
 export const galleryGridHairstyeData = [
   ...Array.from(
-    { length: 51 }, // Adjust the number as needed
+    { length: 59 }, // Adjust the number as needed
     (_, i) => `/images/gallery/hairstyle/gallery(${i}).jpeg`,
   ),
 ];
