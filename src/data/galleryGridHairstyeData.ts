071af99d178c0db33056aa1f4544b6d43625082b
Agata Kamińska <delicadency@gmail.com>
2024-12-17 16:34:15 +0100

    fix: changed gallery array iteration

--- src/data/galleryGridHairstyeData.ts
+++ src/data/galleryGridHairstyeData.ts
@@ -1,6 +1,6 @@
 export const galleryGridHairstyeData = [
   ...Array.from(
     { length: 59 }, // Adjust the number as needed
-    (_, i) => `/images/gallery/hairstyle/gallery(${i}).jpeg`,
+    (_, i) => `/images/gallery/hairstyle/gallery(${58 - i}).jpeg`,
   ),
 ];
